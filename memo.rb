d7644b928c128db5e6787027af10725c433c121b
shimonhayashi <hayashi.shimon@donuts.ne.jp>
2022-11-13 14:19:14 +0900

    指摘箇所の修正1回目

--- memo.rb
+++ memo.rb
@@ -9,8 +9,16 @@ helpers do
     Rack::Utils.escape_html(text)
   end
 
-  def get_file_path(id)
+  def get_saved_memo_path(id)
     "data/memos_#{id}.json"
+  end
+
+  def read_memo_json
+    file_path = get_saved_memo_path(params[:id])
+    File.exist?(file_path) ? (memos = File.open(file_path) { |file| JSON.parse(file.read) }) : (redirect to('not_found'))
+    @title = memos['title']
+    @content = memos['content']
+    @id = memos['id']
   end
 end
 
@@ -35,25 +43,17 @@ get '/memos/new' do
 end
 
 get '/memos/:id' do
-  file_path = get_file_path(params[:id])
-  File.exist?(file_path) ? (memos = File.open(file_path) { |file| JSON.parse(file.read) }) : (redirect to('not_found'))
-  @title = memos['title']
-  @content = memos['content']
-  @id = memos['id']
+  read_memo_json
   erb :detail
 end
 
 get '/memos/:id/edit' do
-  file_path = get_file_path(params[:id])
-  File.exist?(file_path) ? (memos = File.open(file_path) { |file| JSON.parse(file.read) }) : (redirect to('not_found'))
-  @title = memos['title']
-  @content = memos['content']
-  @id = memos['id']
+  read_memo_json
   erb :edit
 end
 
 patch '/memos/:id/edit' do
-  file_path = get_file_path(params[:id])
+  file_path = get_saved_memo_path(params[:id])
   File.open(file_path, 'w') do |file|
     memos = { 'id' => params[:id], 'title' => params[:title], 'content' => params[:content], 'time' => Time.now }
     JSON.dump(memos, file)
@@ -62,7 +62,7 @@ patch '/memos/:id/edit' do
 end
 
 delete '/memos/:id' do
-  file_path = get_file_path(params[:id])
+  file_path = get_saved_memo_path(params[:id])
   File.delete(file_path)
   redirect to('/memos')
 end
